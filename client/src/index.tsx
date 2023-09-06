1bec95b9a0360acc65c4d12a61631a0265b64a46
abdulla <abdullaf2019@outlook.com>
2023-09-06 08:15:33 +0200

    Adding basic redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./app/layout/styles.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./app/router/Routes";
-import { StoreProvider } from "./app/context/StoreContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './app/layout/styles.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
+import { StoreProvider } from './app/context/StoreContext';
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <StoreProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StoreProvider>
   </React.StrictMode>
 );
